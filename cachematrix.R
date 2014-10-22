59a336d8d0cbe5d2b53d92f3982a3e42401e73bd
Ben Faga <bfaga@dhcp80ff7be9.dynamic.uiowa.edu>
2014-10-22 15:58:55 -0500

    Code working need to do that comment thing

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverse <- NULL
+  set <- function(x) {
+    matrix <<- x
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(temp_inverse) inverse <<- temp_inverse
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+
 }
 
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse <- x$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setinverse(inverse)
+  return(inverse)
+
 }
